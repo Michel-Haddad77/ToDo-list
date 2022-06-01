f07df8f425319c526f1e802b1f241077a2ae078e
Micho-PC <michelhdd27@gmail.com>
2022-06-01 19:00:12 +0300

    comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//function that returns the current time and date
 function currentDate(){
     let date = new Date();
     let time = date.getHours() + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
@@ -12,10 +13,13 @@ function generateId(){
     return Math.floor(Math.random()*1000);
 }
 
-
-$("button").click(function(){
+//when the user clicks the create button
+$("#create").click(function(){
     // $("p").hide();
-    let title = $("#title").val();
-    let desc = $("#description").val();
-    $(".todo-container").append('<div class="todo">' + title + '</div><div class="todo">' + desc + '</div><div class="todo"></div>')
+    // let title = $("#title").val();
+    // let desc = $("#description").val();
+    // $("table").append('<div class="todo">' + title + '</div><div class="todo">' + desc + '</div><div class="todo"></div>')
+
+    $(this).css("background-color","yellow");
   });
+
