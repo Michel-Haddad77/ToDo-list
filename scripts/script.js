e1f753b5eacfa63202ea8f50ca53487c528ad6a9
Micho-PC <michelhdd27@gmail.com>
2022-06-01 19:48:23 +0300

    finished create todo function

--- scripts/script.js
+++ scripts/script.js
@@ -19,9 +19,52 @@ function createTodo(){
     let row = $("<tr></tr>");
     $("table").append(row);
 
+    //create id section
     let id_td = $("<td></td");
     id_td.text("id 5"); //to be changed to generated id
     row.append(id_td);
+
+    //create title section
+    let title_td = $("<td></td");
+    title_td.text("id 5"); //to be changed to title
+    row.append(title_td);
+
+    //create description section
+    let description_td = $("<td></td");
+    description_td.text("id 5"); //to be changed to description
+    row.append(description_td);
+    
+    //create priority section
+    let priority_td = $("<td></td");
+    priority_td.text("id 5"); //to be changed to priority
+    row.append(priority_td);
+
+    //create date section
+    let created_at_td = $("<td></td");
+    created_at_td.text(currentDate());
+    row.append(created_at_td);
+
+    //create done? section
+    let done_td = $("<td></td");
+    done_td.text("id 5"); //to be changed to done(yes,no)
+    row.append(done_td);
+
+    //create  row for edit, delete, done buttons
+    let buttons_td = $("<td></td");
+    row.append(buttons_td);
+
+    //create edite ,delete ,done buttons
+    let edit_btn = $("<i></i>");
+    edit_btn.addClass("fa fa-edit fa-xl");
+    buttons_td.append(edit_btn);
+
+    let delete_btn = $("<i></i>");
+    delete_btn.addClass("fa fa-trash fa-xl");
+    buttons_td.append(delete_btn);
+
+    let done_btn = $("<i></i>");
+    done_btn.addClass("fa fa-check fa-xl");
+    buttons_td.append(done_btn);
 
 }
 
